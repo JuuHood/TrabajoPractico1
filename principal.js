1a2abe30f2549e58d478bf5b787cd58c124ce639
JuuHood <juuventre@gmail.com>
2025-04-10 21:08:49 -0300

    Parte 1: Acceso a elementos y recorrido Arrays

--- principal.js
+++ principal.js
@@ -5,4 +5,23 @@ const productos = [
     { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
     { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
     ];
+
+    // Parte 1: Acceso a elementos y recorrido de arrays
+
+    console.log("1. Acceder a propiedades individuales")
+    console.log("Nombre del primer producto: " , productos[0].nombre);
+
+    console.log("Recorrido con for...of: ")
+    for(const producto of productos){
+
+        console.log("Producto: " , producto.nombre, "- Precio: $", producto.precio )
+
+    }
+
+    console.log("3. Recorrido con .forEach():")
+
+    productos.forEach(producto => {
+
+            console.log("Producto: " , producto.nombre, "- Precio: $", producto.precio)
+    })
     
